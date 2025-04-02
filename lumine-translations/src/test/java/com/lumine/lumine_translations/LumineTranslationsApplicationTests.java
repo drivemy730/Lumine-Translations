4805bebfdebf8458100de3d6e6df5c6577055716
David Velez <drivemy730@gmail.com>
2025-04-01 21:07:19 -0500

    initial spring boot project setup

---
+++ lumine-translations/src/test/java/com/lumine/lumine_translations/LumineTranslationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumine.lumine_translations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumineTranslationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
