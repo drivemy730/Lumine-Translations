42da05ff76a843140e566348b43fcade3d6e1357
David Velez <ah.dvelez@BUWELO.local>
2025-04-16 18:48:27 -0500

    created first entity, user

---
+++ Lumine-Translations-main/src/main/java/com/lumine/lumine_translations/controller/UserRole.java
@@ -0,0 +1,12 @@
+package com.lumine.lumine_translations.controller;
+
+public class UserRole
+{
+    public enum Role
+    {
+        CLIENT,
+        TRANSLATOR,
+        ADMIN
+    }
+
+}
---
+++ Lumine-Translations-main/src/main/java/com/lumine/lumine_translations/models/Documents.java
@@ -0,0 +1,26 @@
+package com.lumine.lumine_translations.models;
+
+import java.util.UUID;
+
+public class Documents
+{
+
+    private UUID  documentId;
+
+
+    private Integer clientId;
+
+    private  String s3Key;
+
+    private String fileName;
+
+    private String fileType;
+
+    private Integer wordCount;
+
+    private String sourceLanguage;
+
+    private String targetLanguage;
+
+
+}
