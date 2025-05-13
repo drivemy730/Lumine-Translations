bd2066c503e6b740c9e92140a091ff0893b7868b
David Vélez Álvarez <144290919+drivemy730@users.noreply.github.com>
2025-05-12 19:00:09 -0500

    Update LanguageIsoCode.java
    added enum info

--- backend/src/main/java/helpers/LanguageIsoCode.java
+++ backend/src/main/java/helpers/LanguageIsoCode.java
@@ -1,8 +1,36 @@
-package helpers;
+package com.lumine.lumine_translations.helpers;
 
-public enum LanguageIsoCode
-{
+enum LanguageIsoCode {
+    // English Variants
+    EN("en"),
+    EN_US("en-US"),
+    EN_GB("en-GB"),
+    EN_AU("en-AU"),
+    EN_CA("en-CA"),
+    EN_IN("en-IN"),
 
+    // Spanish Variants
+    ES("es"),
+    ES_ES("es-ES"),
+    ES_MX("es-MX"),
+    ES_AR("es-AR"),
+    ES_CO("es-CO"),
+    ES_US("es-US"),
 
+    // French Variants
+    FR("fr"),
+    FR_FR("fr-FR"),
+    FR_CA("fr-CA"),
+    FR_BE("fr-BE"),
+    FR_CH("fr-CH");
 
+    private final String code;
+
+    LanguageIsoCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
