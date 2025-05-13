201bb4d8fa9dc44772a52dee28dd97bc28cc9cbf
David Vélez Álvarez <144290919+drivemy730@users.noreply.github.com>
2025-05-12 19:00:32 -0500

    Update LanguageName.java
    added language enum info

--- backend/src/main/java/helpers/LanguageName.java
+++ backend/src/main/java/helpers/LanguageName.java
@@ -1,6 +1,26 @@
-package helpers;
+package com.lumine.lumine_translations.helpers;
 
-public enum LanguageName
-{
+enum LanguageName {
+    // English Variants
+    ENGLISH,
+    AMERICAN_ENGLISH,
+    BRITISH_ENGLISH,
+    AUSTRALIAN_ENGLISH,
+    CANADIAN_ENGLISH,
+    INDIAN_ENGLISH,
 
+    // Spanish Variants
+    SPANISH,
+    SPANISH_SPAIN,
+    SPANISH_MEXICO,
+    SPANISH_ARGENTINA,
+    SPANISH_COLOMBIA,
+    SPANISH_US,
+
+    // French Variants
+    FRENCH,
+    FRENCH_FRANCE,
+    FRENCH_CANADA,
+    FRENCH_BELGIUM,
+    FRENCH_SWITZERLAND
 }
